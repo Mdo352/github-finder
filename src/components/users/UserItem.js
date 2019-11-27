f59cb79a192184b00d3a3842f3eb95b049bda58a
Mike <mjeanjacques90@gmail.com>
2019-11-26 23:44:13 -0500

    set userItem proptypes

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,35 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class userItems extends Component {
-    // state = {
-    //     id: 'id',
-    //     login: 'Mdo352',
-    //     avatar_url: 'https://avatars3.githubusercontent.com/u/46452469?s=460&v=4',
-    //     html_url: 'https://github.com/Mdo352'
-    // }
+const UserItem = (props) => {
 
-    // constructor() {
-    //     super();
-    //     // console.log("constructor function")
-    //     this.state = {
-    //         id: 'id',
-    //         login: 'Mdo352',
-    //         avatar_url: 'https://avatars3.githubusercontent.com/u/46452469?s=460&v=4',
-    //         html_url: 'https://github.com/Mdo352'
-    //     }
-    // }
+    const {avatar_url, login, html_url} = props.user;
 
-    render() {
-        const {avatar_url, login, html_url} = this.props.user;
-
-        return (
-            <div className='card text-center' >
-                <img src={avatar_url} alt="User Avatar" className="round-img" style={{ width: '60px' }}/>
-                <h2>{login}</h2>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">View Profile</a>
-            </div>
-        )
-    }
+    return (
+        <div className='card text-center' >
+            <img src={avatar_url} alt="User Avatar" className="round-img" style={{ width: '60px' }}/>
+            <h2>{login}</h2>
+            <a href={html_url} className="btn btn-dark btn-sm my-1">View Profile</a>
+        </div>
+    )
 }
 
-export default userItems
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
+export default UserItem
