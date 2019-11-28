7c3f0945d5e73a705240f62b32ddaec38816c9dc
Mike <mjeanjacques90@gmail.com>
2019-11-28 16:07:04 -0500

    set all users to app level state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,11 @@ import React, { Component } from 'react';
 import UserItem from "./UserItem";
 
 class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'Mdo352',
-                avatar_url: 'https://avatars3.githubusercontent.com/u/46452469?s=460&v=4',
-                html_url: 'https://github.com/Mdo352'
-            },
-            {
-                id: '2',
-                login: 'defunkt',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-                html_url: 'https://github.com/defunkt'
-            },
-            {
-                id: '3',
-                login: 'Modjo',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/Modjo'
-            }
-        ]
-    };
 
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem className='card text-center' key={user.id} user={user} />
                 ))}
             </div>
@@ -40,7 +18,6 @@ const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
-
 }
 
 export default Users
