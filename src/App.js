2608003497f5e9a6383899092b27f28aea5fac9d
Mike <mjeanjacques90@gmail.com>
2019-11-29 20:17:21 -0500

    search component working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -13,7 +14,9 @@ class App extends Component {
   async componentDidMount(){
     this.setState({ loading: true });
     
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_SECRET}`);
     // console.log(res.data);
 
     this.setState({
@@ -28,6 +31,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
